4fe59586e99ed64d0c700652e0b3757f5ebe8e35
Jesse Vaughn <jcv221@gmail.com>
2020-09-28 18:57:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file, emotion)
  #binding.pry
 end
 end 
-return "Sorry, that emoticon was not found"
+"Sorry, that emoticon was not found"
 end 
 
 def get_english_meaning(file, emoticon)

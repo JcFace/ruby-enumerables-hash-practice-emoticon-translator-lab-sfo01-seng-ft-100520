6b37984f285bc6c16b6acf2c37037e1b9ae3e63a
Jesse Vaughn <jcv221@gmail.com>
2020-09-28 18:07:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ nihon_emoticons = YAML.load_file(file)
 nihon_emoticons.each_with_object({}) do |(key, value),
 new_file| 
 value.each do |emotion, translation|
- if !new_filep[key]
+ if !new_file[key]
    new_file[key] = {}
 end 
 end

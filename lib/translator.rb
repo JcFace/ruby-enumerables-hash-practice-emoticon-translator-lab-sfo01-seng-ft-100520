2c43c5a0aa5691aebaf9d3970d75f0ecbdab600b
Jesse Vaughn <jcv221@gmail.com>
2020-09-28 17:30:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+require "yaml"
+require 'pry'
+nihon_emoticons = YAML.load_file(../lib/emoticons.yml)
 
-def load_library
-  # code goes here
-end
+def load_library(nihon_emoticons)
+library = {}
+binding.pry
+end 
 
 def get_japanese_emoticon
-  # code goes here
+ 
 end
 
 def get_english_meaning
   # code goes here
 end
+
+#nihon_emoticons.each_with_object({}) do |(key, value),
+#nihon_new| 
+#value.each do |emotion, translation|
+ # if !nihon_new[key]
+   # nihon_new[key] = {}
+#end
+#end
+#end
